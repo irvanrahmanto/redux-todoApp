fdfb341b51f4a2f3809829a0d288e0de5e6475f6
irvan <irvan.rahmanto@hanabank.co.id>
2024-07-18 22:54:33 +0700

    feat: update action type redux

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,4 +1,4 @@
-import { ADD_TODO } from "./actionTypes";
+import { ADD_TODO, FILTER_TODOS, MARK_ALL_COMPLETED, MARK_COMPLETED, MARK_INCOMPLETE, REMOVE_TODO, UPDATA_SEARCH_TERM } from "./actionTypes";
 
 export const addTodo = (text) => ({
     type: ADD_TODO,
@@ -11,5 +11,47 @@ export const toggleTodo = (id) => ({
     type: ADD_TODO,
     payload: {
         id
+    }
+});
+
+export const removeTodo = (id) => ({
+    type: REMOVE_TODO,
+    payload: {
+        id
+    }
+});
+
+export const markCompleted = (id) => ({
+    type: MARK_COMPLETED,
+    payload: {
+        id
+    }
+});
+
+export const markIncompleted = (id) => ({
+    type: MARK_INCOMPLETE,
+    payload: {
+        id
+    }
+});
+
+export const filterTodo = (filter) => ({
+    type: FILTER_TODOS,
+    payload: {
+        filter
+    }
+});
+
+export const markAllCompleted = () => ({
+    type: MARK_ALL_COMPLETED,
+    payload: {
+        filter
+    }
+});
+
+export const updateSearchTerm = (searchTerm) => ({
+    type: UPDATA_SEARCH_TERM,
+    payload: {
+        searchTerm
     }
 });
