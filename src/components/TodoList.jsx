f830bf4f52617baaa36d30594af2ff5caa55969b
irvan <irvan.rahmanto123@gmail.com>
2024-07-19 16:35:44 +0700

    feat: finish the components

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,27 +1,33 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-    const filteredTodos = useSelector((state) => {
-        const todos = state.todos;
-        const filter = state.filter;
-        const searchTerm = state.searchTerm;
-        return todos.filter((todo) => {
-            const matchesFilter = (filter === "COMPLETED" && todo.completed) || (filter === "INCOMPLETE" && !todo.completed) || (filter === "ALL");
-            const matchesSearch = todo.text.toLowerCase().includes(searchTerm);
+  const filteredTodos = useSelector((state) => {
+    const todos = state.todos;
+    const filter = state.filter;
+    const searchTerm = state.searchTerm;
+    return todos.filter((todo) => {
+      const matchesFilter =
+        (filter === "COMPLETED" && todo.completed) ||
+        (filter === "INCOMPLETE" && !todo.completed) ||
+        filter === "ALL";
+      const matchesSearch = todo.text.toLowerCase().includes(searchTerm);
 
-            return matchesFilter && matchesSearch
-        });
-    })
+      return matchesFilter && matchesSearch;
+    });
+  });
 
-    console.log(">>>>>", filteredTodos)
+  /** print the store */
+//   console.log(">>>>>", filteredTodos);
+
   return (
     <ul>
-        <li className="my-2 text-sm italic">All Your Notes here...</li>
-        {filteredTodos.map((todo, index) => (
-            <li key={index}>{todo.text}</li>
-        ))}
+      <li className="my-2 text-sm italic">All Your Notes here...</li>
+      {filteredTodos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} index={index} />
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
